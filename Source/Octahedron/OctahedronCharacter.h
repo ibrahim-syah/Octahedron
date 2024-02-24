24390bda156a21ec4e6f8c9b6a7b1f11b44125d0
ibrahim-syah <ibrahim.syah.q@gmail.com>
2024-02-24 12:09:54 +0700

    restart

--- Source/Octahedron/OctahedronCharacter.h
+++ Source/Octahedron/OctahedronCharacter.h
@@ -156,6 +156,7 @@ public:
 	USkeletalMeshComponent* GetMesh1P() const { return Mesh1P; }
 	/** Returns FirstPersonCameraComponent subobject **/
 	UCameraComponent* GetFirstPersonCameraComponent() const { return FirstPersonCameraComponent; }
+	float GetDipAlpha() const { return DipAlpha; }
 
 protected:
 	FTimerHandle UnCrouchTimerHandle;
